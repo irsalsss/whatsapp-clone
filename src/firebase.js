323178b6b56a15e141c8a7eba7601b9820270350
Muhamad Irsal Firanyah <muhamadirsal@gmail.com>
2021-01-20 22:05:12 +0700

    import to app js and remove unused file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBW8F_6tQxOHc0e_ZYQHuiorLLvxyFeHIU",
+  authDomain: "whatsapp-mern-54fca.firebaseapp.com",
+  projectId: "whatsapp-mern-54fca",
+  storageBucket: "whatsapp-mern-54fca.appspot.com",
+  messagingSenderId: "697191317438",
+  appId: "1:697191317438:web:ba8568e4d7b8ea07b403ef"
+};
